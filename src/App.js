35f1ec6936e9aca78c1fd4f6ba206e6f1475cfae
Manav <manav@Manavs-MacBook-Pro.local>
2024-08-07 21:48:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import DarkMode from "./DarkMode";
+function App() {
+  return (
+    <div className='App'>
+      this is a good page
+      <DarkMode />
+    </div>
+  );
+}
+
+export default App;
